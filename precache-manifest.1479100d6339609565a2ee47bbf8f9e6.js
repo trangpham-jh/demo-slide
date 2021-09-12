b6d4cb9400639679eba3dbfc9ce3d2bedf555d1d
trangpham-jh <trang.pham@journeyh.io>
2021-09-12 21:40:25 +0700

    Updates

---
+++ precache-manifest.1479100d6339609565a2ee47bbf8f9e6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cfd6c7c3d593b73beaaf2b40f77f33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb831ad0bf531c21ee03",
+    "url": "./static/css/main.bee54973.chunk.css"
+  },
+  {
+    "revision": "de700563c2647070eb92",
+    "url": "./static/js/2.112b893c.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.112b893c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb831ad0bf531c21ee03",
+    "url": "./static/js/main.ca2cce4a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.ca2cce4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c6a44da242a439ab4f",
+    "url": "./static/js/runtime-main.4445e1ad.js"
+  },
+  {
+    "revision": "e4d482a858a86dddb24f983c9f7d48fc",
+    "url": "./static/media/demo1.e4d482a8.png"
+  },
+  {
+    "revision": "96fb2e75d49e49064e0486bb9c261c8b",
+    "url": "./static/media/demo10.96fb2e75.jpg"
+  },
+  {
+    "revision": "734166aeec1caa66a82d2cd536db5efc",
+    "url": "./static/media/demo2.734166ae.jpg"
+  },
+  {
+    "revision": "d71ca5137eec5769cfc43608c45b2938",
+    "url": "./static/media/demo3.d71ca513.jpg"
+  },
+  {
+    "revision": "98abdcd425d20ac2b432f55b1b07bee2",
+    "url": "./static/media/demo4.98abdcd4.jpg"
+  },
+  {
+    "revision": "d32216d04c110a4d1f539efa42ad239d",
+    "url": "./static/media/demo5.d32216d0.jpg"
+  },
+  {
+    "revision": "9a2d5e5f88432603da2b32f4c0f25fb4",
+    "url": "./static/media/demo6.9a2d5e5f.jpg"
+  },
+  {
+    "revision": "25845b38f97dda6a89c9582acc0daa3a",
+    "url": "./static/media/demo7.25845b38.jpg"
+  },
+  {
+    "revision": "367920e9871a32952fee2f166cc33075",
+    "url": "./static/media/demo8.367920e9.jpg"
+  },
+  {
+    "revision": "9ed2ac249c4272adb872f43a8ee784fb",
+    "url": "./static/media/demo9.9ed2ac24.jpg"
+  }
+]);
